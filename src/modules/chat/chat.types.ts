545f81108803b107e1822cbc2bcbfb69f3524760
servorx <191423123+servorx@users.noreply.github.com>
2026-01-21 14:25:41 -0500

    feat: :recycle: big refactor
    se refactoriza toda la parte de la arquitectura del proyecto y se implementan las interfaces, DTOs y repositorios del proyecto pero falta revisar y validar todo estos metodos con swagger

--- src/modules/chat/chat.types.ts
+++
@@ -1,9 +0,0 @@
-export interface IChat {
-  id: bigint;
-  mode: number | null;
-  clientId: bigint | null;
-}
-
-export type CreateChatDTO = IChat;
-export type UpdateChatDTO = Partial<Omit<IChat, "id">> & { id: bigint };
-export type DeleteChatDTO = { id: bigint };
--- src/modules/pqrs/pqrs.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createPqrs } from "./pqrs.controller";
-import { botMiddleware } from "@/middlewares/bot.middleware";
-
-const router = Router();
-
-router.post("/", botMiddleware, createPqrs);
-
-export default router;
