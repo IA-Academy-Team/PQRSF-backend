e07bc3e00d90bb650cf1853066809f3d73c36038
servorx <191423123+servorx@users.noreply.github.com>
2026-02-05 10:14:27 -0500

    feat: :bug: fix prisma generated

--- prisma/PrismaClient.ts
+++ prisma/PrismaClient.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '../generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config';
+import { DATABASE_URL } from '../src/config/env.config.ts';
 
 const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL || '' 
+  connectionString: DATABASE_URL
 });
 
 export const prisma = new PrismaClient({ adapter });
